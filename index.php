820cc55b294b26035c3c70f29ef5688059ca3597
Eduardo Lira <eduardolirainfo@gmail.com>
2017-07-02 20:01:25 -0300

    up

---
+++ index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <title>Dog Of War | Equipe de Muay Thai</title>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="Splash and Coming Soon Page Effects with CSS3" />
+        <meta name="keywords" content="coming soon, splash page, css3, animation, effect, web design" />
+        <meta name="author" content="@dudulira"/>
+        <link rel="shortcut icon" href="../favicon.ico"> 
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons|Open+Sans+Condensed|Oswald" rel="stylesheet">
+  <link rel="stylesheet" href="css/materialize.css">
+
+
+  <!--[if IE]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+    </head>
+    <body>
+  <div class="preloader">
+  <div class="status center"><img data-src="images/dow2.png" class="responsive-img circle wow flip" data-wow-iteration="10" data-wow-duration="2s" ><h6>BEM VINDO</h6></div>
+</div>
+
+    <header><nav class="transparent z-depth-0">
+
+<div class="nav-wrapper">
+  <div class="topoSocial fixed-action-btn toolbar wow fadeInRight"  data-wow-delay="5s">
+    <a class="btn-floating btn-large transparent z-depth-5 pulse waves-effect waves-light">
+      <i class="large material-icons grey-text">share</i>
+    </a>
+    <ul>
+      <li class="waves-effect waves-red tooltipped"  data-position="bottom" data-delay="50" data-tooltip="Google Plus"><a href="#!"><i class="large material-icons red-text"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="#d34836" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M19.5,12H18V10.5H17V12H15.5V13H17V14.5H18V13H19.5V12M9.65,11.36V12.9H12.22C12.09,13.54 11.45,14.83 9.65,14.83C8.11,14.83 6.89,13.54 6.89,12C6.89,10.46 8.11,9.17 9.65,9.17C10.55,9.17 11.13,9.56 11.45,9.88L12.67,8.72C11.9,7.95 10.87,7.5 9.65,7.5C7.14,7.5 5.15,9.5 5.15,12C5.15,14.5 7.14,16.5 9.65,16.5C12.22,16.5 13.96,14.7 13.96,12.13C13.96,11.81 13.96,11.61 13.89,11.36H9.65Z" />
+</svg></i></a></li>
+      <li class="waves-effect waves-light tooltipped" data-position="bottom" data-delay="50" data-tooltip="facebook"><a target="_blank" href="https://www.facebook.com/equipe.dog.of.war/"><i class="large material-icons grey-text">
+<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="#3b5998" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z" />
+</svg></i></a></li>
+      <li class="waves-effect waves-light tooltipped" data-position="bottom" data-delay="50" data-tooltip="Twitter"><a href="#!"><i class="large material-icons grey-text"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="#00aced" d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10 2.38,10 2.38,10.03C2.38,12.11 3.86,13.85 5.82,14.24C5.46,14.34 5.08,14.39 4.69,14.39C4.42,14.39 4.15,14.36 3.89,14.31C4.43,16 6,17.26 7.89,17.29C6.43,18.45 4.58,19.13 2.56,19.13C2.22,19.13 1.88,19.11 1.54,19.07C3.44,20.29 5.7,21 8.12,21C16,21 20.33,14.46 20.33,8.79C20.33,8.6 20.33,8.42 20.32,8.23C21.16,7.63 21.88,6.87 22.46,6Z" />
+</svg></i></a></li>
+      <li class="waves-effect waves-red tooltipped" data-position="bottom" data-delay="50" data-tooltip="YouTube"><a href="#!"><i class="large material-icons red-text"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="#cc181e" d="M10,16.5V7.5L16,12M20,4.4C19.4,4.2 15.7,4 12,4C8.3,4 4.6,4.19 4,4.38C2.44,4.9 2,8.4 2,12C2,15.59 2.44,19.1 4,19.61C4.6,19.81 8.3,20 12,20C15.7,20 19.4,19.81 20,19.61C21.56,19.1 22,15.59 22,12C22,8.4 21.56,4.91 20,4.4Z" />
+</svg></i></a></li>
+    </ul>
+  </div>
+    </div>
+    </nav></header>
+    <aside></aside>
+<main>
+
+<section class="p15 row slider fullscreen valign-wrapper center  white-text">
+<div class="container">
+<div class="moving-zone">
+    <div class="popup">
+        <div class="popup-content">
+            <div class="popup-text">
+                        <a class="link link--takiri container wow fadeInDown" data-wow-delay="3s" href="#dow">
+          <h1><b>DOG OF WAR</b></h1> 
+<span class="wow pulse amber-text transparent" data-wow-iteration="infinite" data-wow-duration="1500ms">Equipe de Muay Thai</span>
+<div class="t1">
+        <div class="in"></div>
+      </div>
+      <div class="t2">
+        <div class="in"></div>
+      </div>
+</a>
+            </div>
+        </div>
+    </div>
+</div>
+</div>  
+
+
+<ul class="slides">
+      <li>
+<img class="responsive-img" src="images/tenor.gif" data-src="images/lira.jpg" alt="">
+      </li>
+      <li>
+<img src="images/tenor.gif" data-src="images/mt.jpg" alt="">
+      </li>
+      <li>
+<img src="images/tenor.gif" data-src="images/globe.png" alt="">
+      </li>
+    </ul>
+</section>
+<br/><br/>
+
+
+  <div id="parceiros" class="fixed-action-btn vertical wow fadeInRight hide-on-med-and-down"  data-wow-delay="5s">
+    <a class="btn-floating btn-large transparent z-depth-5 waves-effect waves-light tooltipped" data-tooltip="Parceiros" data-position="left" data-delay="50">
+      <i class="grey-text large material-icons" >free_breakfast</i>
+    </a>
+    <ul>
+      <li><a class="btn-floating white" target="_blank" href="https://www.facebook.com/grupocamim/"> <img class="responsive-img tooltipped" data-src="images/camim-logo.png" data-position="left" data-delay="50" data-tooltip="CAMIM Saúde"></a></li>
+
+      <li><a class="btn-floating white" target="_blank" href="https://www.facebook.com/personalizacaobl/"><img class="responsive-img tooltipped" data-src="images/bl.jpg" data-position="left" data-delay="50" data-tooltip="BL Personalização"></a></li>
+
+      <li><a class="btn-floating white" target="_blank" href="https://www.facebook.com/AcademiaR2fit/"><img class="responsive-img tooltipped" data-position="left" data-delay="50" data-tooltip="R2 Academia" data-src="images/r2.jpg">
+</a></li>
+      <li><a class="btn-floating white " target="_blank" href="https://www.facebook.com/Jofaflex-Coberturas-e-Serralheria-em-geral-1663615473965837/">  <img class="responsive-img tooltipped" data-position="left" data-delay="50" data-tooltip="JOFAFLEX Coberturas e Serralheria" data-src="images/jfx.jpg">
+</a></li>
+<li><a class="btn-floating white" target="_blank" href="https://www.facebook.com/LETAL-RJ-768552356597618/">  <img class="responsive-img tooltipped"  data-position="left" data-delay="50" data-tooltip="Letal RJ" data-src="images/letalrj.png">
+</a></li>
+
+
+
+    </ul>
+  </div>
+        
+
+<div class="down grey center hide-on-med-and-down"><p>"Tudo posso naquele me fortalece" - Filipenses 4:13</p></div>
+</main>
+  
+  <!-- Modal Structure -->
+  <div id="dow" class="modal black white-text">
+    <div class="modal-content">
+    <a href="#!" class="right modal-action modal-close waves-effect waves-red btn-flat"><i class="white-text large material-icons">clear</i></a>
+    <div class="container">
+    <h5 class="row center"><span class="black-text white">Dog of War </span>| <span class="amber">Equipe de Muay Thai</span></h5>
+      <div class="section center">
+      <h5>sobre</h5>
+        <h6>Uma introdução sobre nosso trabalho</h6>
+        <hr>
+     
+         <div class="row">
+          <div class="col s12 m4 l4">
+            <h6>História</h6>
+            <p>Em breve um pouco da nossa história</p>
+          </div>
+          <div class="col s12 m4 l4">
+            <h6>Perfil</h6>
+            <div class="card red darken-1">
+              <div class="card-content white-text">
+                <img src="images/dow2.png" width="64" height="64">
+                <p>Em breve</p>
+              </div>
+              <div class="card-action">
+              
+              </div>
+            </div>
+          </div>
+          <div class="col s12 m4 l4">
+            <h6>Projetos</h6>
+            <ul class="collapsible grey-text">
+              <li class="">
+                <div class="collapsible-header"><i class="mdi-av-web"></i>Em breve</div>
+                <div class="collapsible-body"><p>Em breve</p></div>
+              </li>
+              <li>
+                <div class="collapsible-header"><i class="mdi-editor-format-align-justify"></i>Em breve</div>
+                <div class="collapsible-body"><p>Em breve</p></div>
+              </li>
+              <li>
+                <div class="collapsible-header"><i class="mdi-av-play-shopping-bag"></i>Em breve</div>
+                <div class="collapsible-body"><p>Em breve</p></div>
+              </li>
+              <li>
+                <div class="collapsible-header"><i class="mdi-editor-insert-comment"></i>Em breve</div>
+                <div class="collapsible-body"><p>Em breve</p></div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      </div>
+      </div>
+    </div>
+
+    </body>
+<link href="https://unpkg.com/animate.css@3.5.1/animate.min.css" rel="stylesheet" type="text/css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
+"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js"></script>
+
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+<script type="text/javascript">
+  
+new WOW().init();
+
+
+
+$( document ).ready(function() {
+
+Materialize.fadeInImage('img');
+$('.modal').modal({startingTop: '0%',endingTop: '0%'});
+$('.slides').hide();
+$('.tooltipped').tooltip();
+$('.materialboxed').materialbox();
+       // will first fade out the loading animation
+  $(".status").delay(3000).fadeOut("slow");
+        // will fade out the whole DIV that covers the website.
+  $(".preloader").delay(3000).fadeOut("slow");
+
+  $(".down").delay(5000).fadeOut();
+
+  var allimages= document.getElementsByTagName('img');
+    for (var i=0; i<allimages.length; i++) {
+        if (allimages[i].getAttribute('data-src')) {
+            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+        }
+    }
+$( "#parceiros" ).hover(
+  function() {
+    $('.slides').show();
+    $('.slider').slider({'fullWidth':true, 'indicators': false, 'trasition':100, 'interval':1000});
+  }, function() {
+   $('.slider').slider('pause');
+   $('.slides').fadeOut("slow");
+
+  }
+);
+
+
+
+var moveForce = 30; // max popup movement in pixels
+var rotateForce = 20; // max popup rotation in deg
+
+$(document).mousemove(function(e) {
+    var docX = $(document).width();
+    var docY = $(document).height();
+    
+    var moveX = (e.pageX - docX/2) / (docX/2) * -moveForce;
+    var moveY = (e.pageY - docY/2) / (docY/2) * -moveForce;
+    
+    var rotateY = (e.pageX / docX * rotateForce*2) - rotateForce;
+    var rotateX = -((e.pageY / docY * rotateForce*2) - rotateForce);
+    
+    $('.popup')
+        .css('left', moveX+'px')
+        .css('top', moveY+'px')
+        .css('transform', 'rotateX('+rotateX+'deg) rotateY('+rotateY+'deg)');
+});
+});
+</script>
+</html>
