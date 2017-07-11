003c3bd7d43ba19bb0d573ec03a5d074c9c6e40b
Eduardo Lira <eduardolirainfo@gmail.com>
2017-07-11 20:55:49 -0300

    up

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+	// declarar as nossas dependências
+var gulp = require('gulp'),
+	svgSprite = require('gulp-svg-sprite'),
+	plumber     = require('gulp-plumber'),
+	stylus      = require('gulp-stylus'),
+	uglify      = require('gulp-uglify'),
+	concat      = require('gulp-concat'),
+	jeet        = require('jeet'),
+	rupture     = require('rupture'),
+	koutoSwiss  = require('kouto-swiss'),
+	prefixer    = require('autoprefixer-stylus'),
+	imagemin    = require('gulp-imagemin'),
+	cp          = require('child_process');
+
+
+var config = {
+    mode: {
+        symbol: {
+            dest: 'assets/icons',
+            sprite: 'sprite.svg',
+            example: true
+        }
+    },
+    svg: {
+        xmlDeclaration: false,
+        doctypeDeclaration: false
+    }
+};
+
+
+
+
+
+/**
+ * Stylus task
+ */
+gulp.task('stylus', function(){
+		gulp.src('src/styl/main.styl')
+		.pipe(plumber())
+		.pipe(stylus({
+			use:[koutoSwiss(), prefixer(), jeet(),rupture()],
+			compress: true
+		}))
+		.pipe(gulp.dest('assets/css'))
+});
+
+/**
+ * Javascript Task
+ */
+gulp.task('js', function(){
+	return gulp.src('src/js/**/*.js')
+		.pipe(plumber())
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('assets/js/'))
+});
+
+/**
+ * Imagemin Task
+ */
+gulp.task('imagemin', function() {
+	return gulp.src('src/img/**/*.{jpg,png,gif}')
+		.pipe(plumber())
+		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+		.pipe(gulp.dest('assets/img/'));
+});
+
+/**
+ * Watch stylus files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+	gulp.watch('src/styl/**/*.styl', ['stylus']);
+	gulp.watch('src/js/**/*.js', ['js']);
+	gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
+	gulp.watch(['*.html']);
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('sprites', function(){
+    return gulp.src('src/icons/**/*.svg')
+        .pipe(svgSprite(config))
+        .pipe(gulp.dest('.'))
+});
+
+
+gulp.task('default', ['js', 'stylus', 'sprites', 'watch', 'imagemin']);
